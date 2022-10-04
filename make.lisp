01004648099963501331569231c4a1be08fa3cfc
Artyom Bologov <aartaka@protonmail.com>
2022-10-04 22:20:03 +0400

    make: Auto-generate repl.bf and repl.min.bf.

--- make.lisp
+++ make.lisp
@@ -21,3 +21,28 @@ Basically, Brainfuck commands you use.")
   (loop for char across (uiop:read-file-string #p"mbfl.bf")
         when (find char commands)
           do (write-char char min-file)))
+
+(with-open-file (repl-file #p"repl.bf"
+                           :direction :output
+                           :if-exists :supersede
+                           :if-does-not-exist :create)
+  (write-string "- ; set N to 255
+>>>> ; instructions
+,----- ----- [>,----------] ; read until a newline
+<[+++++ +++++<] ; restore the values
+>; move to instruction
+[<<<< ; N
+ " repl-file)
+  (write-string (uiop:read-file-string #p"mbfl.bf") repl-file)
+  (write-string "
+ >>>>[>]<[[-]<]> ; Clear the previous input
+ ,----- ----- [>,----- -----] <[+++++ +++++<]> ; read a new one
+]" repl-file))
+
+(with-open-file (min-file #p"repl.min.bf"
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+  (loop for char across (uiop:read-file-string #p"repl.bf")
+        when (find char commands)
+          do (write-char char min-file)))
