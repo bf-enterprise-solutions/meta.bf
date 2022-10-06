a9c34c76d3288eff3cc6be5fa536e39cf5413ebe
Artyom Bologov <aartaka@protonmail.com>
2022-10-06 23:58:52 +0400

    make: Add oneshot.bf for one-off evaluation.

--- make.lisp
+++ make.lisp
@@ -22,18 +22,37 @@ Basically, Brainfuck commands you use.")
         when (find char commands)
           do (write-char char min-file)))
 
+(with-open-file (oneshot-file #p"oneshot.bf"
+                              :direction :output
+                              :if-exists :supersede
+                              :if-does-not-exist :create)
+  (write-string ">>>----- ; set N to 254
+>>>> ; instructions
+,----- ----- [>,----------] ; read until a newline
+<[+++++ +++++<] ; restore the values
+<<< ; N" oneshot-file)
+  (write-string (uiop:read-file-string #p"mbfr.bf") oneshot-file))
+
+(with-open-file (min-file #p"oneshot.min.bf"
+                          :direction :output
+                          :if-exists :supersede
+                          :if-does-not-exist :create)
+  (loop for char across (uiop:read-file-string #p"oneshot.bf")
+        when (find char commands)
+          do (write-char char min-file)))
+
 (with-open-file (repl-file #p"repl.bf"
                            :direction :output
                            :if-exists :supersede
                            :if-does-not-exist :create)
-  (write-string "- ; set N to 255
+  (write-string ">>>----- ; set N to 254
 >>>> ; instructions
 ,----- ----- [>,----------] ; read until a newline
 <[+++++ +++++<] ; restore the values
 >; move to instruction
 [<<<< ; N
  " repl-file)
-  (write-string (uiop:read-file-string #p"mbfl.bf") repl-file)
+  (write-string (uiop:read-file-string #p"mbfr.bf") repl-file)
   (write-string "
  >>>>[>]<[[-]<]> ; Clear the previous input
  ,----- ----- [>,----- -----] <[+++++ +++++<]> ; read a new one
